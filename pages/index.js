011f44017c4391bb6d321a0bddb189148a205c8e
ivajo26 <ivajodimo26@gmail.com>
2019-07-26 07:23:25 -0500

    Set Main Layout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
 const Index = () => (
-  <h1>Get Start!</h1>
+  <div className="Layout">
+    <div className="Layout-sidebar">
+      <div className="Sidebar-logo">
+        <img src="" alt=""/>
+      </div>
+      <div className="Sidebar-options">
+
+      </div>
+    </div>
+    <div className="Layout-main">
+      <nav className="Main-nav">
+        <button>
+          <i></i>
+          <span>Home</span>
+        </button>
+      </nav>
+      <section className="Main-content">
+
+      </section>
+    </div>
+  </div>
 )
 
 export default Index
