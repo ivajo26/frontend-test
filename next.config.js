262068ca96bcf9f456e8a304dda11796ed418bd5
ivajo26 <ivajodimo26@gmail.com>
2019-07-27 17:22:01 -0500

    Add general styles

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 
 const withStylus = require('@zeit/next-stylus')
 const withESLint = require('next-eslint')
+const autoprefixer = require('autoprefixer-stylus')
+const nib = require('nib')
 
-module.exports = withStylus(withESLint())
+
+module.exports = withStylus(withESLint(
+  {
+    stylusLoaderOptions: {
+      use: [nib(), autoprefixer({hideWarnings: true})]
+    },
+  }
+))
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import '../styles/index.styl'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLightbulb } from '@fortawesome/free-regular-svg-icons'
 
 const Index = () => (
   <div className="Layout">
     <div className="Layout-sidebar">
       <div className="Sidebar-logo">
-        <img src="" alt=""/>
+        <img src="/static/logo.png" alt="logo"/>
       </div>
       <div className="Sidebar-options">
-
+        <h5 className="text font-weight-normal"><FontAwesomeIcon icon={faLightbulb}/> Smart Filters</h5>
+        <hr className="divider"/>
+        <hr className="divider"/>
+        <h5 className="text font-weight-normal">Status</h5>
       </div>
     </div>
     <div className="Layout-main">
